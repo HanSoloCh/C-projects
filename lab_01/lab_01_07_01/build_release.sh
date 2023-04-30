8fbf432d26c5085c6628cc8d252853eb052fd434
Novikov Artem <novikovaa6@student.bmstu.ru>
2023-04-30 15:52:37 +0300

    clened

--- lab_01/lab_01_07_01/build_release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Проверяем, запущен ли скрипт из его собственной директории
-if [[ $(realpath "$(dirname "$0")") != "$PWD" ]]; then
-	echo "Please run script from its own direcory"
-else
-	gcc -std=c99 -Wall -Werror -Wpedantic -Wextra -Wfloat-equal -Wfloat-conversion \
-	-Wvla -c -O3 main.c
-	gcc main.o -o app.exe -lm
-fi
